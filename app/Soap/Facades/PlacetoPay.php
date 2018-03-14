5416ccef28a5e51e19c8be1d57641e9b356f5279
jhfaber <jfmarina@unal.edu.co>
2018-03-14 18:43:28 -0500

    base

---
+++ app/Soap/Facades/PlacetoPay.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Soap\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Class PlacetoPay to take advantage of Laravel Container
+ *
+ * @package App\Soap\Facades
+ */
+class PlacetoPay extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return PlacetoPay::class;
+    }
+}
